6df6c9653c782e7515418201482540cb96b6fb47
eric <eric@kramer.esso.ucar.edu>
2018-01-22 09:55:15 -0700

    Latest updates to spatial and text search. Split up functionality. Got Leaflet to work properly with JQwidgets.

--- src/js/geodex/geodexspatialsearch.js
+++ src/js/geodex/geodexspatialsearch.js
@@ -1,6 +1,14 @@
-function createComponents(){
-    
-    //SPATIAL SEARCH COMPONENTS///////////////////////////////
+function initializeSpatialSearchPanel(){
+	
+    //Create Leaflet Map
+    var mymap = L.map('spatialSearchMap').setView([51.505, -0.09], 13);
+    L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoiZWxpbmdlcmYiLCJhIjoiY2pjamhlZTl1NGRxazJxbzU0OHE5d3ZxNyJ9.n7A_BBoZxnpA2izU3McwSQ', {
+        attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
+        maxZoom: 18,
+        id: 'mapbox.streets',
+        accessToken: 'pk.eyJ1IjoiZWxpbmdlcmYiLCJhIjoiY2pjamhlZTl1NGRxazJxbzU0OHE5d3ZxNyJ9.n7A_BBoZxnpA2izU3McwSQ'
+    }).addTo(mymap);
+	
     //Create buttons
     $("#spatialSearchRemoveButton").jqxButton({ width: 300, height: 40 });
 	$("#spatialSearchAddButton").jqxButton({ width: 300, height: 40 });
@@ -8,27 +16,18 @@ function createComponents(){
     $("#spatialSearchSubmitButton").jqxButton({ width: 300, height: 40 });
     
     //Create fields for location entry
-    $("#spatialSearchLatField").jqxInput({height: 40, width: 200});
+   	$("#spatialSearchLatField").jqxInput({height: 40, width: 200});
     $("#spatialSearchLonField").jqxInput({height: 40, width: 200});
     
-    //Create list for holding locations
+     //Create list for holding locations
     $("#spatialSearchListBox").jqxListBox({width: 200, height: 250 });
-    
-    //Create Leaflet Map
-    var mymap = L.map('spatialSearchMap').setView([51.505, -0.09], 13);
-    L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoiZWxpbmdlcmYiLCJhIjoiY2pjamhlZTl1NGRxazJxbzU0OHE5d3ZxNyJ9.n7A_BBoZxnpA2izU3McwSQ', {
-        attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
-        maxZoom: 18,
-        id: 'mapbox.streets',
-        accessToken: 'your.mapbox.access.token'
-    }).addTo(mymap);
     
 }
 
 function initialize(){
 	
 	//SPATIAL SEARCH BUTTON FUNCTIONS//////////////////////////
-	$('#spatialSearchUpdateButton').on('click', function (event) {
+	/*$('#spatialSearchUpdateButton').on('click', function (event) {
 
 	});
 	$('#spatialSearchAddButton').on('click', function (event) {
@@ -39,6 +38,6 @@ function initialize(){
 	});
 	$('#spatialSearchSubmitButton').on('click', function (event) {
 
-	});
+	});*/
 	
 }
--- src/js/geodex/geodextextsearch.js
+++ src/js/geodex/geodextextsearch.js
@@ -1,16 +1,10 @@
 function createComponents(){
-    //TEXT SEARCH COMPONENTS//////////////////////////////////
+
     //Create buttons
     $("#textSearchSubmitButton").jqxButton({ width: 300, height: 40 });
     
     //Create text fields
     $("#textSearchValueField").jqxInput({ width: 300, height: 40 });
-    
-  	//Get dimensional properties 
-    //var container = $('#main');
-    //var containerOffset = container.offset();
-    //var containerWidth = container.width();
-    //var containerHeight = container.height();
     
 }
 
