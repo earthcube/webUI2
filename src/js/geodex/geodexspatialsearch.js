6f81b0eb0978af182198ca57b3a9d297826cd253
eric <eric@kramer.esso.ucar.edu>
2018-01-26 09:53:28 -0700

    Added sorting routine for drawing the polygon region.

--- src/js/geodex/geodexspatialsearch.js
+++ src/js/geodex/geodexspatialsearch.js
@@ -1,10 +1,18 @@
+//Global variables
+var marker;
+var oldMarkers = [];
+var oldLines = [];
+var map;
+var currentLocation;
+var upper;
+
 function initializeSpatialSearchPanel(){
 	
 	//Set initial location to Boulder, CO.
-	var currentLocation = [40.0150, -105.2705]
+	currentLocation = [40.0150, -105.2705]
 	
     //Create Leaflet Map
-    var map = L.map('spatialSearchMap').setView(currentLocation, 13);
+    map = L.map('spatialSearchMap').setView(currentLocation, 13);
     L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoiZWxpbmdlcmYiLCJhIjoiY2pjamhlZTl1NGRxazJxbzU0OHE5d3ZxNyJ9.n7A_BBoZxnpA2izU3McwSQ', {
         attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
         maxZoom: 18,
@@ -22,11 +30,16 @@ function initializeSpatialSearchPanel(){
 	    	shadowSize: [41, 41]
     });
 
-    var marker = L.marker(currentLocation, {icon: greenIcon}).addTo(map);
+    //Add marker to map
+    marker = L.marker(currentLocation, {icon: greenIcon}).addTo(map);
     
     //Create fields for location entry
    	$("#spatialSearchLatField").jqxInput({height: 40, width: '100%'});
     $("#spatialSearchLonField").jqxInput({height: 40, width: '100%'});
+    
+    //Initialize field values 
+    $("#spatialSearchLatField").jqxInput('val', currentLocation[0]);
+    $("#spatialSearchLonField").jqxInput('val', currentLocation[1]);
     
     //Create buttons
     $("#spatialSearchUpdateButton").jqxButton({ width: '100%', height: 40 });
@@ -40,27 +53,218 @@ function initializeSpatialSearchPanel(){
 	$("#spatialSearchRemoveButton").on("click", spatialSearchRemoveButtonClicked);
 	$("#spatialSearchSubmitButton").on("click", spatialSearchSubmitButtonClicked);
 	
-	//IMPORTANT! This line of javascript must go last in this function or other JS breaks after this is called
 	//Create list for holding locations
     $("#spatialSearchListBox").jqxListBox({height: '100%', width: '100%'});
-    
-    
 	
 }
 
 function spatialSearchUpdateButtonClicked(){
-	$("#spatialSearchLatField").jqxInput('val', 'HELP!');
+	//Update the location of the current marker
+	var lat = $("#spatialSearchLatField").jqxInput('val');
+	var lon = $("#spatialSearchLonField").jqxInput('val');
+	currentLocation = [lat, lon];
+	var latlng = L.latLng(lat, lon);
+	marker.setLatLng(latlng);
+	redrawMap();
 }
 
 function spatialSearchAddButtonClicked(){
-	
+	//Add the current location
+	var lat = $("#spatialSearchLatField").jqxInput('val');
+	var lon = $("#spatialSearchLonField").jqxInput('val');
+	$("#spatialSearchListBox").jqxListBox('addItem', lat + ", " + lon);
+	redrawMap();
 }
 
 function spatialSearchRemoveButtonClicked(){
-	
+	//Remove the selected location
+	var item = $("#spatialSearchListBox").jqxListBox('getSelectedItem'); 
+	$("#spatialSearchListBox").jqxListBox('removeItem', item);
+	redrawMap();
 }
 
 function spatialSearchSubmitButtonClicked(){
+	//Go get data!
+}
+
+
+
+function setFitBounds(){
+
+	//Calculate a bounding box that will include all markers
+	var lat = $("#spatialSearchLatField").jqxInput('val');
+	var lon = $("#spatialSearchLonField").jqxInput('val');
+	
+	neLocationLat = parseFloat(lat);
+    neLocationLon = parseFloat(lon);
+    swLocationLat = parseFloat(lat);
+    swLocationLon = parseFloat(lon);
+    
+    var locations = $("#spatialSearchListBox").jqxListBox('getItems');
+    
+    if(locations){
+    
+	    for(i = 0; i<locations.length; i++){
+			
+			var location = locations[i];
+			var locationLabel = location.label;
+			var locationLabelArray = locationLabel.split(",");
+			var lat = parseFloat(locationLabelArray[0]);
+			var lon = parseFloat(locationLabelArray[1]);
+			
+			if (lat < swLocationLat){
+				swLocationLat = parseFloat(lat);
+			}
+			if (lon < swLocationLon){
+				swLocationLon = parseFloat(lon);
+			}
+	
+			if (lat > neLocationLat){
+				neLocationLat = parseFloat(lat);
+			}
+			if (lon > neLocationLon){
+				neLocationLon = parseFloat(lon);
+			}
+			
+	    }
+    
+    }
+    
+    swLocation = [swLocationLat, swLocationLon]
+    neLocation = [neLocationLat, neLocationLon]
+    
+    map.fitBounds([swLocation, neLocation]);
+    
+}
+
+function redrawMap(){
+
+	//Remove all current markers
+	for(i = 0; i<oldMarkers.length; i++){
+		map.removeLayer(oldMarkers[i]);
+	}
+	
+	//Remove all current lines
+	for(i = 0; i<oldLines.length; i++){
+		map.removeLayer(oldLines[i]);
+	}
+	
+	//Create new markers for each location
+	var locations = $("#spatialSearchListBox").jqxListBox('getItems');
+	
+	if(locations){
+	
+		for(i = 0; i<locations.length; i++){
+			
+			var location = locations[i];
+			var locationLabel = location.label;
+			var locationLabelArray = locationLabel.split(",");
+			var lat = locationLabelArray[0];
+			var lon = locationLabelArray[1];
+	
+			var marker = L.marker([lat, lon]).addTo(map);
+			oldMarkers.push(marker);
+			
+		}
+		
+		//Draw lines and polygons
+		if(locations.length>1){
+			
+			var latlngs = [];
+			var points = [];
+			
+			for(i = 0; i<locations.length; i++){
+				
+				var location = locations[i];
+				var locationLabel = location.label;
+				var locationLabelArray = locationLabel.split(",");
+				var lat = parseFloat(locationLabelArray[0]);
+				var lon = parseFloat(locationLabelArray[1]);
+		
+				points.push(new Point(lat, lon));
+				
+			}
+			
+			upper = upperLeft(points);
+			points.sort(pointSort);
+			
+			for(i = 0; i<points.length; i++){
+				var latlng = L.latLng(parseFloat(points[i].lat), parseFloat(points[i].lon));
+				latlngs.push(latlng);
+			}
+			latlng = L.latLng(parseFloat(points[0].lat), parseFloat(points[0].lon));
+			latlngs.push(latlng);
+			
+			var polyline = L.polyline(latlngs).addTo(map);
+			oldLines.push(polyline);
+			
+		}
+	
+	}
+	
+	setFitBounds();
 	
 }
 
+// A representation of a location.
+function Point(lat, lon) {
+
+	
+	this.lat = lat;
+	this.lon = lon;
+    this.x = (lon + 180) * 360;
+    this.y = (lat + 90) * 180;
+
+    this.distance=function(that) {
+        var dX = that.x - this.x;
+        var dY = that.y - this.y;
+        return Math.sqrt((dX*dX) + (dY*dY));
+    }
+
+    this.slope=function(that) {
+        var dX = that.x - this.x;
+        var dY = that.y - this.y;
+        return dY / dX;
+    }
+}
+
+// A custom sort function that sorts p1 and p2 based on their slope
+// that is formed from the upper most point from the array of points.
+function pointSort(p1, p2) {
+	
+    // Exclude the 'upper' point from the sort (which should come first).
+    if(p1 == upper) return -1;
+    if(p2 == upper) return 1;
+
+    // Find the slopes of 'p1' and 'p2' when a line is 
+    // drawn from those points through the 'upper' point.
+    var m1 = upper.slope(p1);
+    var m2 = upper.slope(p2);
+
+    // 'p1' and 'p2' are on the same line towards 'upper'.
+    if(m1 == m2) {
+        // The point closest to 'upper' will come first.
+        return p1.distance(upper) < p2.distance(upper) ? -1 : 1;
+    }
+
+    // If 'p1' is to the right of 'upper' and 'p2' is the the left.
+    if(m1 <= 0 && m2 > 0) return -1;
+
+    // If 'p1' is to the left of 'upper' and 'p2' is the the right.
+    if(m1 > 0 && m2 <= 0) return 1;
+
+    // It seems that both slopes are either positive, or negative.
+    return m1 > m2 ? -1 : 1;
+}
+
+// Find the upper most point. In case of a tie, get the left most point.
+function upperLeft(points) {
+    var top = points[0];
+    for(var i = 1; i < points.length; i++) {
+        var temp = points[i];
+        if(temp.y > top.y || (temp.y == top.y && temp.x < top.x)) {
+            top = temp;
+        }
+    }
+    return top;
+}
